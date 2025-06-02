7f0e78c38a5cfc9429cdb4df1a90226283a16493
Steven Cannon <scannon@iastate.edu>
2025-06-02 12:05:10 -0500

    Fix error in top-match selection

--- bin/gfa.sh
+++ bin/gfa.sh
@@ -159,7 +159,7 @@ for filepath in 01_hmmsearch/*.hmmsearch.tbl; do
   file=$(basename "$filepath" .hmmsearch.tbl)
   base=$(echo "$file" | perl -pe 's/(.+)\.\w+/$1/') # strips suffix like .protein_primary or .protein
   awk -v OFS="\t" '$1!~/^#/ {print $1, $3, $1, $5, $6}' "$filepath" |
-    sort -k1,1 -k6nr,6nr | top_line.awk |
+    sort -k1,1 -k5nr,5nr | top_line.awk |
       strip_spliceform.pl > 02_gfa/"${base}"."${hmmdb}".gfa.tsv &
    # allow to execute up to $NPROC in parallel
    if [[ $(jobs -r -p | wc -l) -ge ${NPROC} ]]; then wait -n; fi
