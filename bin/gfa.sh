fee52824a0131872ae76143b99c33ed67aa93dc3
Steven Cannon <scannon@iastate.edu>
2025-06-03 13:18:14 -0500

    Swap columns 1 and 3 in output. Col 1 is now gene ID and col 3 is transcript (protein) ID.

--- bin/gfa.sh
+++ bin/gfa.sh
@@ -158,9 +158,11 @@ echo
 for filepath in 01_hmmsearch/*.hmmsearch.tbl; do
   file=$(basename "$filepath" .hmmsearch.tbl)
   base=$(echo "$file" | perl -pe 's/(.+)\.\w+/$1/') # strips suffix like .protein_primary or .protein
+  cat /dev/null > 02_gfa/"${base}"."${hmmdb}".gfa.tsv
+  printf "#gene\tfamily\tprotein\te-value\tscore\n" > 02_gfa/"${base}"."${hmmdb}".gfa.tsv
   awk -v OFS="\t" '$1!~/^#/ {print $1, $3, $1, $5, $6}' "$filepath" |
     sort -k1,1 -k5nr,5nr | top_line.awk |
-      strip_spliceform.pl > 02_gfa/"${base}"."${hmmdb}".gfa.tsv &
+      strip_spliceform.pl >> 02_gfa/"${base}"."${hmmdb}".gfa.tsv &
    # allow to execute up to $NPROC in parallel
    if [[ $(jobs -r -p | wc -l) -ge ${NPROC} ]]; then wait -n; fi
 done
